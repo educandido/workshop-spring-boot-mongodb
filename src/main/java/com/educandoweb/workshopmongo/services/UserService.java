1b70ab4a925c91338e49ca83f8feb934d0250b10
educandido <educandido@gmail.com>
2020-09-29 23:46:58 -0300

    Inserção de Usuário com POST

--- src/main/java/com/educandoweb/workshopmongo/services/UserService.java
+++ src/main/java/com/educandoweb/workshopmongo/services/UserService.java
@@ -1,12 +1,12 @@
 package com.educandoweb.workshopmongo.services;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.workshopmongo.domain.User;
+import com.educandoweb.workshopmongo.dto.UserDTO;
 import com.educandoweb.workshopmongo.repository.UserRepository;
 import com.educandoweb.workshopmongo.services.exceptions.ObjectNotFoundException;
 
@@ -25,4 +25,11 @@ public class UserService {
 		return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto Não encontrado"));
 	}
 	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+	}
 }
