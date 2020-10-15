d0fb7acd531cfa1d8864e52c59869a7bae3fa30b
educandido <educandido@gmail.com>
2020-10-14 22:38:17 -0300

    Obtendo um post por id

---
+++ src/main/java/com/educandoweb/workshopmongo/services/PostService.java
@@ -0,0 +1,20 @@
+package com.educandoweb.workshopmongo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.workshopmongo.domain.Post;
+import com.educandoweb.workshopmongo.repository.PostRepository;
+import com.educandoweb.workshopmongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto Não encontrado"));
+	}
+	
+}
